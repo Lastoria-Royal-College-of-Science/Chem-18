ba4a4ac6ab9cefc66eae31b81941035e32e4f057
y-oyama-lrcs <y.oyama@lrcs.ac>
2025-10-06 22:31:10 -0400

    Lesson 4 CLT Exercise revised & finished

---
+++ Lesson 4/clt.py
@@ -0,0 +1,67 @@
+"""
+**You should learn Lesson 1.5 and preferably 3.5 before check this script!**
+
+The use of decorator here may be confusing, but it is an elegant way to implement
+interactive widgets.
+
+This script provides an interactive visualization of the relationship between
+the histogram of uniformly distributed random variables and the normal
+distribution of their mean values. It allows users to dynamically adjust the
+number of uniform random variables and view how the overall distribution
+changes.
+
+Functions:
+    - make_plot(n: int): Generates and displays the plots comparing the uniform
+      random variables' histogram and the corresponding normal distribution.
+"""
+
+
+from ipywidgets import interact
+import numpy as np
+from scipy.stats import norm
+from matplotlib import pyplot as plt
+
+
+@interact(n=(1, 30, 1))
+def make_plot(n: int = 1):
+    """
+    Generates and displays plots comparing the histogram of uniform random variables
+    and their corresponding normal distribution across multiple subplots. The number
+    of random variables can be adjusted using the parameter.
+
+    :param n: The number of columns in the uniform random matrix. This determines
+        the number of random variables whose means will be calculated and plotted.
+    :type n: int
+    :return: None
+    """
+    rm_array = np.random.rand(1000000, n).mean(axis=1)
+
+    mean, std = rm_array.mean(), rm_array.std()
+    x_min, x_max = mean - 5*std, mean + 5*std
+    norm_x = np.linspace(x_min, x_max, 1000)
+
+    plt.figure(figsize=(10, 6))
+
+    plt.subplot(2,2,(1,2))
+    plt.hist(rm_array, bins='auto', range=(x_min, x_max), density=True, histtype='stepfilled', label='Random Variables')
+    plt.plot(norm_x, norm.pdf(norm_x, loc=mean, scale=std), label='Normal Distribution')
+    plt.xlim(x_min, x_max)
+    plt.legend()
+    plt.title('Uniform Distribution and Its Associated Normal Distribution')
+
+    plt.subplot(2,2,3)
+    plt.hist(rm_array, bins='auto', range=(x_min, x_max), density=True, histtype='stepfilled', label='Random Variables')
+    plt.plot(norm_x, norm.pdf(norm_x, loc=mean, scale=std), label='Normal Distribution')
+    plt.xlim(x_min-4*std, x_max-4*std)
+    plt.ylim(0, 0.002)
+    plt.legend()
+
+    plt.subplot(2,2,4)
+    plt.hist(rm_array, bins='auto', range=(x_min, x_max), density=True, histtype='stepfilled', label='Random Variables')
+    plt.plot(norm_x, norm.pdf(norm_x, loc=mean, scale=std), label='Normal Distribution')
+    plt.xlim(x_min+4*std, x_max+4*std)
+    plt.ylim(0, 0.002)
+    plt.legend()
+
+    plt.tight_layout()
+    plt.show()
